20674e6cd331aae8fa878921ca5f3703dda4dc1e
Taniel Rémi <taniel.remi@gmail.com>
2020-07-18 16:21:35 +0200

    :bug: fix jpa auditing

--- src/main/java/com/where2beer/ws/common/model/Auditable.java
+++ src/main/java/com/where2beer/ws/common/model/Auditable.java
@@ -3,12 +3,17 @@ package com.where2beer.ws.common.model;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import java.util.Date;
 
 @Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class Auditable {
 
     @CreatedDate
